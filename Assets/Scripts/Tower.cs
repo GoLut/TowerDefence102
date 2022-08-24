404b22e1fad5c6b95f7ea6afbede34a3f0fb5090
GoLut <jjlut99@gmail.com>
2022-08-24 21:52:51 +0200

    Targeting of enemies.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,13 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     private SpriteRenderer mySpriteRenderer;
+
+    //the current target enemy of the tower.
+    private Enemy target;
     
+    //queue of monsters that are in the towers range
+    private Queue<Enemy> enemies = new Queue<Enemy>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +22,8 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+        Debug.Log(target);
     }
 
     //toggle select the sprite renderer.
@@ -24,4 +32,33 @@ public class Tower : MonoBehaviour
         mySpriteRenderer.enabled = !mySpriteRenderer.enabled;
     }
     
+    //attack the target enemy.
+    private void Attack()
+    {
+        if (target == null&& enemies.Count > 0)
+        {
+            //selets the first target from the list.
+            target = enemies.Dequeue();
+        }
+    }
+    
+    //when ever an enemy enters the range circle of the tower.
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //enque the enemies into the queue
+            enemies.Enqueue(other.GetComponent<Enemy>());
+            
+        }
+    }
+
+    //when the target exits the circle eg. dies or is to far away.
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            target = null;
+        }
+    }
 }
